2509584b6f302cf4372a526510f04b218b2d2fc6
Unknown <loris.zirah@etu.umontpellier.fr>
2017-05-20 19:40:57 +0200

    Test connexion bd

--- controller/verifInscriptionJoueur.controller.php
+++ controller/verifInscriptionJoueur.controller.php
@@ -3,7 +3,7 @@ require_once('../model/connexionBD.php');
 require_once('../model/Joueur.php');
 
 
-   //TODO : mettre ces variables dans un fichier .env
+  //TODO : mettre ces variables dans un fichier .env
   $key = "vOlleYYBallzTournAm1ntOrgAN1SatAurE";
   $keyCryptage= "ProJa1TWeib";
 
--- model/connexionBD.php
+++ model/connexionBD.php
@@ -1,12 +1,15 @@
 <?php
 try
 	{
-		$pdo = new PDO('pgsql:user=feoilwsqqlyeaz dbname=dc73ovmdsu7c1o charset=utf8 password=76f373700f07860ad6b2086111b0964b3ec05e40b2c2fd492f9c89c842804b5c');
+		$host_bd='ec2-46-137-97-169.eu-west-1.compute.amazonaws.com';
+		$name_bd='dc73ovmdsu7c1o';
+		$user_bd='feoilwsqqlyeaz';
+		$pass_bd='76f373700f07860ad6b2086111b0964b3ec05e40b2c2fd492f9c89c842804b5c';
+		$pdo = new PDO("pgsql:host=".$host_bd.";dbname=".$name_bd."", "".$user_bd."","".$pass_bd."");
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(Exception $e)
 	{
 	        die('Erreur : '.$e->getMessage());
-
 	}
 ?>
