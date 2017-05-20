70dfd5b1d18522e8f417af59dec894daf2332c06
Unknown <loris.zirah@etu.umontpellier.fr>
2017-05-20 20:49:18 +0200

    test connexion bd

---
+++ controller/connexiontest.controller.php
@@ -0,0 +1,7 @@
+<?php
+require_once('../model/connexionBD.php');
+require_once('../model/Joueur.php');
+
+$idPsoeudo=existeJoueur("Airkan");
+include('../view/connexiontest.php');
+?>
---
+++ view/connexiontest.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Volley-Ball tournament organizator</title>
+		<!-- BOOTSTRAP STYLES-->
+		<link href="../assets/css/bootstrap.css" rel="stylesheet" />
+		<!-- FONTAWESOME STYLES-->
+		<link href="../assets/css/font-awesome.css" rel="stylesheet" />
+		<!-- CUSTOM STYLES-->
+		<link href="../assets/css/perso.css" rel="stylesheet" />
+		<link href="../assets/css/custom.css" rel="stylesheet" />
+		<link href="../assets/css/general.css" rel="stylesheet" />
+		<!-- GOOGLE FONTS-->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+	</head>
+	<body>
+    <?php echo $idPsoeudo ?>
+
+    <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME -->
+    <!-- JQUERY SCRIPTS -->
+    <script src="../assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="../assets/js/jquery.metisMenu.js"></script>
+    <!-- CUSTOM SCRIPTS -->
+    <script src="../controller/js/connexion.js"></script>
+    <script src="../assets/js/custom.js"></script>
+  </body>
+  </html>
