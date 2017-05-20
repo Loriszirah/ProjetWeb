e6f70f9eb0c6a7acd23daed6d6eda17545ca59bf
Unknown <loris.zirah@etu.umontpellier.fr>
2017-05-20 21:22:31 +0200

    connexion test

--- controller/connexiontest.controller.php
+++ controller/connexiontest.controller.php
@@ -1,5 +1,4 @@
 <?php
-require_once('../model/connexionBD.php');
 require_once('../model/Joueur.php');
 
 $idPsoeudo=existeJoueur('Airkan');
--- model/joueur.php
+++ model/joueur.php
@@ -1,6 +1,6 @@
 <?php
 //fonctions d'accès a la base de données du type joueur
-
+$pdo=new PDO("pgsql:dbname='dc73ovmdsu7c1o';host='ec2-46-137-97-169.eu-west-1.compute.amazonaws.com','feoilwsqqlyeaz','76f373700f07860ad6b2086111b0964b3ec05e40b2c2fd492f9c89c842804b5c'");
 
 function existeJoueur($idJoueur){
 	//donnée : id du joueur
