675d3be1b0c573c0378da923d3400e9aba456d05
Unknown <loris.zirah@etu.umontpellier.fr>
2017-05-20 20:53:14 +0200

    test

--- controller/connexiontest.controller.php
+++ controller/connexiontest.controller.php
@@ -2,6 +2,7 @@
 require_once('../model/connexionBD.php');
 require_once('../model/Joueur.php');
 
-$idPsoeudo=existeJoueur("Airkan");
+connexionBD();
+$idPsoeudo=existeJoueur('Airkan');
 include('../view/connexiontest.php');
 ?>
