01ecffcb673b29ad901d64f741de039a585e1edd
Unknown <loris.zirah@etu.umontpellier.fr>
2017-05-20 13:08:31 +0200

    Modif : verifInscription

--- controller/js/inscriptionVerificationInfo.js
+++ controller/js/inscriptionVerificationInfo.js
@@ -2,39 +2,7 @@ function verifInfo(){
 	/*
 	 * But: On vérifie que toutes les cases sont remplies
 	 */
-	 res=true;
-   if(document.getElementById("nom").value==""){
-		 alert("Veuilez indiquer votre nom");
-		 res=false;
-	 }
-   if(document.getElementById("prenom").value==""){
-		 alert("Veuilez indiquer votre prenom");
-		 res=false;
-	 }
-   if(document.getElementById("email").value==""){
-		 alert("Veuilez indiquer votre email");
-		 res=false;
-	 }
-   if(document.getElementById("psoeudo").value==""){
-		 alert("Veuilez indiquer votre psoeudo");
-		 res=false;
-	 }
-   if(document.getElementById("age").value==""){
-		 alert("Veuilez indiquer votre age");
-		 res=false;
-	 }
-   if(document.getElementById("telephone").value==""){
-		 alert("Veuilez indiquer votre numero de téléphone");
-		 res=false;
-	 }
-   if(document.getElementById("ville").value==""){
-		 alert("Veuilez indiquer votre ville");
-		 res=false;
-	 }
-   if(document.getElementById("passwd").value==""){
-		 alert("Veuilez indiquer un mot de passe");
-		 res=false;
-	 }
+   res=true;
 	 return res;
 }
 
@@ -47,6 +15,7 @@ function verifPassword(){
    password2=document.getElementById("passwdconf").value
    if(password2!=password1){
      res=false;
+     document.getElementById("passwdconf").focus();
    }
    return res;
 }
