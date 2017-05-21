6e90450ae353374783fe65fc27cb1599b47b9d5c
Unknown <loris.zirah@etu.umontpellier.fr>
2017-05-21 16:15:12 +0200

    test recup infos bd

--- model/joueur.php
+++ model/joueur.php
@@ -10,7 +10,7 @@ function getAll(){
 
 		$req=$pdo->prepare('SELECT * FROM person');
 		$req->execute(array());
-		$list=$req->fetch();
+		$list=$req->fetch(PDO::FETCH_ASSOC);
 	} catch(PDOException $e){
 			echo($e->getMessage());
 			die(" Erreur lors de la vérification de l'éxistence du joueur" );
