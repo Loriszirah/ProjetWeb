d17c853438d2a809b371b287c0965e7eb0209c63
Unknown <loris.zirah@etu.umontpellier.fr>
2017-05-23 01:28:51 +0200

    correction file connexionBD

--- model/connexionBD.php
+++ model/connexionBD.php
@@ -1,12 +1,8 @@
 <?php
 try
 	{
-		$host = '';
-		$dbname='';
-		$pseudo='root';
-		$password='';
-		$pdo = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8',$pseudo,$password);
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
++		$pdo = new PDO('pgsql:host=ec2-46-137-97-169.eu-west-1.compute.amazonaws.com;port=5432;dbname=dc73ovmdsu7c1o','feoilwsqqlyeaz','76f373700f07860ad6b2086111b0964b3ec05e40b2c2fd492f9c89c842804b5c');
+ +		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(Exception $e)
 	{
