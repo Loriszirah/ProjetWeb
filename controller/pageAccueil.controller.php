58c458c4cdd9bd60fb86157e1c3c5a882760cc8b
Loriszirah <loris.zirah@etu.umontpellier.fr>
2017-05-23 12:04:04 +0200

    Update pageAccueil.controller.php
    add : init_set

--- controller/pageAccueil.controller.php
+++ controller/pageAccueil.controller.php
@@ -2,6 +2,8 @@
   require_once('../vendor/autoload.php');
   use \Firebase\JWT\JWT;
 
+  ini_set('display_errors',1);
+
    //On vérifie que l'utilisateur est déjà connecté
    if(!isset($_COOKIE["token"])){
           // On le redirige vers la page d'accueil
